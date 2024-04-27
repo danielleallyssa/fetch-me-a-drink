1378a516cb49d95320c0c1afe84e9429e30bbc66
Danielle Brown <hello@danielleallyssa.com>
2024-04-27 01:30:58 -0400

    Added some addition JS functionality and css animations.

--- main.js
+++ main.js
@@ -4,29 +4,39 @@
 let button = document.querySelector("button");
 let last = document.querySelector("#prev");
 let next = document.querySelector("#next");
+let random = document.querySelector("#random");
 
 // Output
 let title = document.querySelector("h2");
 let instructions = document.querySelector(".instructions");
 let image = document.querySelector("img");
+let type = document.querySelector("#drink-type");
+let category = document.querySelector("#drink-category");
+let glass = document.querySelector("#drink-glass");
 let i = 0;
+
+// Events on load
+window.addEventListener("load", getRandomCocktail);
 
 // Events on click
 button.addEventListener("click", getCocktail);
 last.addEventListener("click", goBack);
 next.addEventListener("click", goNext);
+random.addEventListener("click", getRandomCocktail);
 
 // Keyup events
 document.addEventListener("keyup", function (e) {
-  if (e.key === "ArrowLeft") {
+  if (e.code === "ArrowLeft" || e.code === "ArrowDown") {
     goBack();
-  } else if (e.key === "ArrowRight") {
+  } else if (e.code === "ArrowRight" || e.code === "ArrowUp") {
     goNext();
-  } else if (e.key === "Enter") {
+  } else if (e.code === "Enter") {
     getCocktail();
+  } else if (e.code === "Space") {
+    getRandomCocktail();
   }
 
-  //   console.log(e);
+  console.log(e);
 });
 
 // Functions
@@ -35,12 +45,36 @@ function getCocktail() {
   fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${userInput}`)
     .then((res) => res.json()) // parse response as JSON
     .then((data) => {
-      const drinkEl = data.drinks[i];
+      let drinkEl = data.drinks[i];
 
-      //   console.log(data.drinks[i]);
       title.innerText = drinkEl.strDrink;
       image.src = drinkEl.strDrinkThumb;
       instructions.innerText = drinkEl.strInstructions;
+      type.innerText = drinkEl.strAlcoholic;
+      category.innerText = drinkEl.strCategory;
+      glass.innerText = drinkEl.strGlass;
+
+      // console.log(data.drinks[i]);
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
+}
+
+function getRandomCocktail() {
+  fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+    .then((res) => res.json())
+    .then((data) => {
+      // console.log(data);
+
+      const randomDrinkEl = data.drinks[0];
+
+      title.innerText = randomDrinkEl.strDrink;
+      image.src = randomDrinkEl.strDrinkThumb;
+      instructions.innerText = randomDrinkEl.strInstructions;
+      type.innerText = randomDrinkEl.strAlcoholic;
+      category.innerText = randomDrinkEl.strCategory;
+      glass.innerText = randomDrinkEl.strGlass;
     })
     .catch((err) => {
       console.log(`error ${err}`);
@@ -51,6 +85,7 @@ function goBack() {
   i--;
   getCocktail();
 }
+
 function goNext() {
   i++;
   getCocktail();
